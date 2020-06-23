ca903069c7cc24a3554ca2e9859404ffad8f0f06
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2020-06-23 17:46:20 +0600

    Splash screen

---
+++ app/src/main/java/com/example/bloodbank/Activities/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.bloodbank.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //use a handler to creat a delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },2500);
+    }
+}
